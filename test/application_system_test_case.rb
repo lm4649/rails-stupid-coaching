5ad3dfd493820aef7d1657c415076fcd59a316d8
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-03 15:47:41 +0900

    done

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  Capybara.register_driver(:headless_firefox) do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.firefox \
+      firefoxOptions: { args: %w[headless disable-gpu window-size=1280x760] }
+    Capybara::Selenium::Driver.new app,
+      browser: :firefox, desired_capabilities: capabilities
+  end
+  driven_by :selenium, using: :headless_firefox
 end
